c1c4ac12a123fafdc166b6d1f402121497dd2cd9
aCadogan <atimetowait@gmail.com>
2025-12-10 06:31:58 -0500

    Typos mainly

--- content/whats-your-name.tsx
+++ content/whats-your-name.tsx
@@ -5,5 +5,5 @@ export interface WhatsYourName {
 
 export const whatsYourName: WhatsYourName = {
   "title": "WHAT'S YOUR NAME?",
-  "content": "<p>For so long I toiled to find answers in names, people, myself...for that mattter. <br />\n<br />Solitude, maybe solace. \n<br />It lays in honesty, vulnerability — An openness to your own wounds. <br />\n<br /> What is said, cannot be taken back.<br />But those in the process of a doing. <br /> Hold the weight to right a wrong that might just become a new verb, word — function of their own causality to change our paracausal forces...forever.\n<br />\n<br /></p>\n<h3>— A PROMPT LIES BELOW —</h3>\n"
+  "content": "<p>For so long I toiled to find answers in names, people, myself...for that manner. <br />\n<br />Solitude grants maybe, solace. \n<br />It lays in honesty though, vulnerability — An openness to your own wounds. <br />\n<br /> What is said, cannot be taken back.<br /></p>\n<blockquote>\n<p>But those in the process of a doing. <br /> Hold the weight to right a wrong that just might become a new verb, word — a new function of their own causality to change our paracausal forces...forever.\n<br />\n<br /></p>\n</blockquote>\n<h3>— A PROMPT LIES BELOW —</h3>\n"
 }
