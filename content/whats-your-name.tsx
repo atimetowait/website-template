8755c551544076866c8de630d72e7ced284db65c
aCadogan <atimetowait@gmail.com>
2025-12-10 03:58:14 -0500

    Prose fix

--- content/whats-your-name.tsx
+++ content/whats-your-name.tsx
@@ -5,5 +5,5 @@ export interface WhatsYourName {
 
 export const whatsYourName: WhatsYourName = {
   "title": "WHAT'S YOUR NAME?",
-  "content": "<p>For so long I toiled to find answers in names, people, myself...for that mattter. <br />\n<br />Solitude, maybe solace. \n<br />Lies in honesty, vulnerability — An openness to your own wounds. <br />\n<br /> What is said, cannot be taken back.<br />But those in the process of a doing. <br /> Hold weight to right a wrong that might just become a new verb, word — function of their own causality to change our paracausal forces...forever.\n<br />\n<br /></p>\n<h3>— A PROMPT LIES BELOW —</h3>\n"
+  "content": "<p>For so long I toiled to find answers in names, people, myself...for that mattter. <br />\n<br />Solitude, maybe solace. \n<br />It lays in honesty, vulnerability — An openness to your own wounds. <br />\n<br /> What is said, cannot be taken back.<br />But those in the process of a doing. <br /> Hold the weight to right a wrong that might just become a new verb, word — function of their own causality to change our paracausal forces...forever.\n<br />\n<br /></p>\n<h3>— A PROMPT LIES BELOW —</h3>\n"
 }
